0047184e636e64dd1acb5be64269719d25b094c5
Nick Bansal <https://github.com/NickBansal>
2018-12-02 21:02:19 +0000

    All functions tested

--- src/test/utils.test.js
+++ src/test/utils.test.js
@@ -1,4 +1,18 @@
-import { createArray, updatedArray, updateGrid, randomSquareSelection } from '../utils';
+import { 
+    createArray, 
+    updatedArray, 
+    updateGrid, 
+    checkAmountOfNeighbours, 
+    regenerateCells, 
+    Comparator, 
+    removeDuplicates, 
+    bacteria
+} from '../utils';
+
+const grid1 = [[1, 2], [1, 3], [1, 4]]
+const grid2 = [[2, 2], [2, 3], [3, 2], [3, 3]]
+const grid3 = [[1, 2], [1, 3], [1, 4], [1, 5]]
+const remainingCells = [[1, 2], [1, 3], [1, 4], [2, 1], [2, 2]]
 
 describe('Utils testing', () => {
     test('Fake Test', () => {
@@ -33,7 +47,74 @@ describe('Utils testing', () => {
         expect(updatedArray(grid3)).toEqual([[0, 1], [1, 1]])
     })
 
-    // it('Creates a grid with randomly selected items', () => {
-    //     expect(randomSquareSelection(10)).toEqual([])
-    // })
+    // Checking the amount of neighbours for each cell 
+    it('Returns the amount of neighbours of a specified cell', () => {
+        const singleCell = [0, 0]
+        expect(checkAmountOfNeighbours(singleCell, remainingCells)).toEqual(0)
+    })
+    it('Returns the amount of neighbours of a specified cell', () => {
+        const singleCell = [1, 0]
+        expect(checkAmountOfNeighbours(singleCell, remainingCells)).toEqual(1)
+    })
+    it('Returns the amount of neighbours of a specified cell', () => {
+        const singleCell = [1, 1]
+        expect(checkAmountOfNeighbours(singleCell, remainingCells)).toEqual(3)
+    })
+    it('Returns the amount of neighbours of a specified cell', () => {
+        const singleCell = [2, 3]
+        expect(checkAmountOfNeighbours(singleCell, remainingCells)).toEqual(4)
+    })
+
+
+    // Compare arrays within arrays and sort
+    it('Compare arrays within arrays and sort then into order', () => {
+        const array = [[1, 3], [1, 1], [1, 2]]
+        expect(array.sort(Comparator)).toEqual([[1, 1], [1, 2], [1, 3]])
+    })
+    it('Compare arrays within arrays and sort then into order', () => {
+        const array = [[0, 3], [0, 11], [0, 2]]
+        expect(array.sort(Comparator)).toEqual([[0, 2], [0, 3], [0, 11]])
+    })
+    it('Compare arrays within arrays and sort then into order', () => {
+        const array = [[3, 2], [3, 3], [2, 2], [2, 3]]
+        expect(array.sort(Comparator)).toEqual([[2, 2], [2, 3], [3, 2], [3, 3]])
+    })
+
+    // Calculates the regeneration of dead cells
+    it('Cell regeneration to regenerate dead cells with 3 live neighbours', () => {
+        expect(regenerateCells(grid1)).toEqual([[0, 3], [2, 3]])
+    })
+    it('Cell regeneration to regenerate dead cells with 3 live neighbours', () => {
+        expect(regenerateCells(grid2)).toEqual(grid2)
+    })
+    it('Cell regeneration to regenerate dead cells with 3 live neighbours', () => {
+        const answer = [[0, 3], [0, 4], [2, 3], [2, 4]]
+        expect(regenerateCells(grid3)).toEqual(answer)
+    })
+
+    // Removing duplicates from arrays
+    it('Will find duplicates in an array and remove them', () => {
+        expect(removeDuplicates([1, 1, 2, 3])).toEqual([1, 2, 3])
+        expect(removeDuplicates([1, 2, 1, 3, 1])).toEqual([1, 2, 3])
+        expect(removeDuplicates([1, 2, 1, 3, 1, 2, 3])).toEqual([1, 2, 3])
+    })
+
+    // Final bacteria result
+    it('An input will return a new array of live cells', () => {
+        const answer = [[0, 3], [1, 3], [2, 3]]
+        expect(bacteria(grid1)).toEqual(answer)
+    })
+    it('An input will return a new array of live cells', () => {
+        expect(bacteria(grid2)).toEqual(grid2)
+    })
+    it('An input will return a new array of live cells', () => {
+        const finalQuestion = [[1, 2], [2, 2], [3, 2]]
+        const finalAnswer = [[2, 1], [2, 2], [2, 3]]
+        expect(bacteria(finalQuestion)).toEqual(finalAnswer)
+    })
+    it('An input will return a new array of live cells', () => {
+        const finalQuestion = [[2, 1], [2, 2], [2, 3]]
+        const finalAnswer = [[1, 2], [2, 2], [3, 2]]
+        expect(bacteria(finalQuestion)).toEqual(finalAnswer)
+    })
 })
