3da42c296caa61955928cd49e421e7b02b61b970
Nick Bansal <https://github.com/NickBansal>
2018-12-13 15:22:35 +0000

    Testing for main component

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -3,9 +3,28 @@ import './setupTest'
 import App from '../GameOfLife';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json'
+import { fooPattern1, fooPattern2 } from '../utils/examplePatterns'
+import { createEmptyGameBoard, updateGrid } from '../utils/index'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('<App />', () => {
+    let wrapper
+    beforeEach(() => {
+        wrapper = shallow(<App />)
+    })
+    it('Creates snapshots of the snake app', () => {
+        expect(toJson(wrapper)).toMatchSnapshot()
+    })
+    it('Reset button will reset the board', () => {
+        const newInstance = wrapper.instance()
+        const answer = updateGrid(createEmptyGameBoard(30), [...fooPattern1, ...fooPattern2])
+        newInstance.resetBoard()
+        expect(wrapper.state().grid).toEqual(answer)
+    })
+    it('handleClick changes the game state', () => {
+        const newInstance = wrapper.instance()
+        newInstance.handleClick()
+        expect(wrapper.state().game).toBeFalsy()
+        newInstance.handleClick()
+        expect(wrapper.state().game).toBeTruthy()
+    })
+})
